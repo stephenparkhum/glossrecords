06c1749fee45068a1596eba21035d3c5ed2f68da
Stephen P <stephenparkhum@outlook.com>
2018-11-23 21:45:09 -0800

    Updats

--- Web Draft 1/resources/json/artists.js
+++ Web Draft 1/resources/json/artists.js
@@ -4,4 +4,5 @@ var arkhum = {
     "album": "The Skies Do Give Succor", 
     "bio": "Brutal blackened death metal with a science fictional imperative. Releases on Vendlus and Glossolalia. Featured current and former members of Pillorian, Maestus, and Grst. 2006 - 2016"
 
-}
+};
+
