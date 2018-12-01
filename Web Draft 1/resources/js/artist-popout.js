87b67d1e21c25fd0272ef491aca30edd5bddc352
Stephen P <stephenparkhum@outlook.com>
2018-12-01 08:51:24 -0800

    Logo updates + artist popout

--- Web Draft 1/resources/js/artist-popout.js
+++ Web Draft 1/resources/js/artist-popout.js
@@ -1,3 +1,26 @@
-function showDiv() {
-    document.getElementById('artist-popout').style.display = "visible";
- }
+// function ShowDiv() {
+//     document.getElementById("artist-popout").style.display = "";
+// }
+
+// $( "#artist-popout" ).click(function() {
+//     document.getElementById("artist-popout").style.display="visible";
+//   });
+
+$(document).ready(function() {
+    $(".arkhum").click(function() {
+    $("#artist-popout").show();
+    });
+
+    $(".close").click(function() {
+    $("#artist-popout").hide();
+    });
+
+    $(".banewreaker").click(function() {
+    $("#banewreaker").show();
+    });
+
+    $("#banewreaker .close").click(function() {
+    $("#banewreaker").hide();
+    });
+});
+
